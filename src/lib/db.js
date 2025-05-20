d63f9bbc92bfbc3a0e08478cfca0c0bd316f0449
Sruti Bhagavatula <srutib@cmu.edu>
2025-05-20 10:12:31 -0500

    Small update to be consistent with OG guestbook

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 export const getDatabase = () => {
   const db = mysql.createConnection({
-    multipleStatements: false,
-    host     : '8.tcp.ngrok.io',
-    port     : 10041,
+    multipleStatements: true,
+    host     : '0.tcp.ngrok.io',
+    port     : 11916,
     user     : '396user',
     password : '396user', //Not secure to store it this way; fill in yourself
     database : 'guestbook'
